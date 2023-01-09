c7cdb458410606a3cbed6c0142e191f1b63fa398
Trusov <a.trusov@voskhod.ru>
2023-01-09 17:24:07 +0400

    add web driver manager and start first test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
     }
 }
