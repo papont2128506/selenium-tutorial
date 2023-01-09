3662f1540f874802fbc5a307110ec5cc91b1a6a8
Trusov <a.trusov@voskhod.ru>
2023-01-09 19:04:00 +0400

    виды поиска в findElement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +14,19 @@ public class Main {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://google.com");
+        WebElement input = driver.findElement(By.xpath("//input[@aria-label=\"Найти\"]"));
+//        WebElement input = driver.findElement(By.cssSelector());
+//        WebElement input = driver.findElement(By.className()); //атрибут class
+//        WebElement input = driver.findElement(By.id()); //атрибут id
+//        WebElement input = driver.findElement(By.name()); //атрибут name
+//        WebElement input = driver.findElement(By.linkText()); //Текст ссылки
+//        WebElement input = driver.findElement(By.partialLinkText()); //часть текста ссылки
+//        WebElement input = driver.findElement(By.tagName()); //атрибут tagname
+
+        input.click();
+
+        List<WebElement> elements = driver.findElements(By.xpath("//input[@aria-label=\"Найти\"]"));
+
 
     }
 }
